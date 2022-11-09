550dc4eff586f4e9e07df6f428b638a4e61904b3
RevDonz <aprilio842@gmail.com>
2022-11-09 14:38:34 +0700

    add sidebar dashboard

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -27,12 +27,14 @@ const Navbar = () => {
             <div className='rounded-md px-3 py-2 text-[#254A75] transition hover:bg-[#254A75] hover:text-white'>
               Keluarga
             </div>
-            <a
-              href='404.html'
-              className='rounded-md border border-gray-300 px-3 py-2 text-[#307DD1] transition hover:bg-[#307DD1] hover:text-white'
-            >
-              Login / Register
-            </a>
+            <Link href={'/dashboard'}>
+              <button
+                href='404.html'
+                className='rounded-md border border-gray-300 px-3 py-2 text-[#307DD1] transition hover:bg-[#307DD1] hover:text-white'
+              >
+                Login / Register
+              </button>
+            </Link>
           </div>
         </div>
       </div>
