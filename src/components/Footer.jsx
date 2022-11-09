523d04f98bdb84895f8f7b26ae123bf0c752b8e1
RevDonz <aprilio842@gmail.com>
2022-11-09 13:40:11 +0700

    add 404 page | progress dashboard

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,27 +1,26 @@
-import Image from 'next/image'
-import React from 'react'
+import Image from 'next/image';
+import React from 'react';
 
 const Footer = () => {
   return (
     <div className='bg-[#254A75]'>
-        
-        <div className='flex items-center justify-between py-5 mx-auto w-[90%] max-w-screen-xl'>
-          <div className='flex gap-5 items-center'>
-            <Image
-              src={'/logo/logo-sipenca-white.svg'}
-              alt='logo-sipenca'
-              width={40}
-              height={40}
-            />
-            <p className='text-white'>Copyright © 2022 - All right reserved</p>
-          </div>
-          <div className='flex gap-3'>
-            <p className='text-white'>@twitter</p>
-            <p className='text-white'>@instagram</p>
-          </div>
+      <div className='flex items-center justify-between py-5 mx-auto w-[90%] max-w-screen-xl'>
+        <div className='flex gap-5 items-center'>
+          <Image
+            src={'/logo/logo-sipenca-white.svg'}
+            alt='logo-sipenca'
+            width={40}
+            height={40}
+          />
+          <p className='text-white'>Copyright © 2022 - All right reserved</p>
+        </div>
+        <div className='flex gap-3'>
+          <p className='text-white'>@twitter</p>
+          <p className='text-white'>@instagram</p>
         </div>
       </div>
-  )
-}
+    </div>
+  );
+};
 
-export default Footer
+export default Footer;
--- src/pages/dashboard/index.jsx
+++ src/pages/dashboard/index.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React from 'react';
+import Footer from '../../components/Footer';
+import Navbar from '../../components/Navbar';
 
 const Dashboard = () => {
   return (
-    <div>Dashboard</div>
-  )
-}
+    <div className=''>
+      <Navbar />
+      <div className='py-10 bg-[#EFF0F2]'>
+        <div className='flex w-full'>
+          <div className='w-1/3 bg-white'>
+            <p>as</p>
+          </div>
+          <div className='w-2/3 bg-white'>
+            <p>asd</p>
+          </div>
+        </div>
+      </div>
+      <Footer />
+    </div>
+  );
+};
 
-export default Dashboard
+export default Dashboard;
