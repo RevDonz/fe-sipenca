ebd9d386725e293ff079cf7a40210154126fc2de
Padliwinata <rpadliwinata@gmail.com>
2022-11-12 18:35:45 +0700

    pengungsian warga

---
+++ src/pages/pengungsian_warga.jsx
@@ -0,0 +1,15 @@
+import React, { Children } from 'react'
+import Layout from '../components/Layout'
+
+const pengungsian_warga = () => {
+    return (
+        <Layout>
+            <Title>Example</Title>
+            <Children>
+                <h1>Example too</h1>
+            </Children>
+        </Layout>
+    )
+}
+
+export default pengungsian_warga
