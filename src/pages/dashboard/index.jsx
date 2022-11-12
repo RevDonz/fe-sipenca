29bd35667f38914543c4b8704ebe12ac26be37ab
RevDonz <aprilio842@gmail.com>
2022-11-12 09:45:11 +0700

    add list menu

--- src/pages/dashboard/index.jsx
+++ src/pages/dashboard/index.jsx
@@ -1,15 +1,12 @@
 import React from 'react';
-import Footer from '../../components/Footer';
 import Layout from '../../components/Layout';
-import Navbar from '../../components/Navbar';
-import Sidebar from '../../components/Sidebar';
 
 const Dashboard = () => {
   return (
     <Layout title={'Profile'}>
-      <div className='p-5'>
+      {/* <div className='p-5'>
         <h3 className='font-semibold'>Profil</h3>
-      </div>
+      </div> */}
     </Layout>
   );
 };
